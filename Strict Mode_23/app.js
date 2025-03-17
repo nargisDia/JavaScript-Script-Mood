ef556888b825bd9b10f8f9bc6e4cb6b90ea00b4e
nargisDia <126104947+nargisDia@users.noreply.github.com>
2025-03-17 12:26:10 +0600

    Add files via upload

---
+++ Strict Mode_23/app.js
@@ -0,0 +1,28 @@
+// JavaScript Strict Mode
+
+'use strict';
+
+//x = 5;
+let x = 5;
+console.log(x);
+
+// name();
+// function name() {
+//     // y = 5;
+//     let y = 5;
+//     console.log(y);
+// }
+
+name( 5, 4);
+function name( p1, p2) {
+   
+    let y = 5;
+    console.log( p1, p2);
+}
+
+
+//let private = 'some text';
+//let public = 'some text';
+//let package = 'some text';
+
+
